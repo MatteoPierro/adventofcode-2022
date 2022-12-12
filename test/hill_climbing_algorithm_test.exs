d46505aae69ab452f66485c0cd194041a04d4cfa
Matteo Pierro <pierromatteo@gmail.com>
2022-12-12 12:41:13 +0100

    solve first puzzle day 12

--- test/hill_climbing_algorithm_test.exs
+++ test/hill_climbing_algorithm_test.exs
@@ -49,10 +49,10 @@ defmodule HillClimbingAlgorithmTest do
 
   test "valid step" do
     assert valid_step?("a", "b")
-    assert valid_step?("a", "E")
+    refute valid_step?("a", "E")
     refute valid_step?("a", "c")
     assert valid_step?("z", "f")
-    assert valid_step?("S", "f")
+    refute valid_step?("S", "f")
   end
 
   test "priority queue" do
@@ -80,7 +80,6 @@ defmodule HillClimbingAlgorithmTest do
     map = parse_map(@test_map)
 
     path = find_shortest_path(map)
-    IO.inspect(path)
     assert Enum.count(path) - 1 == 31
   end
 
@@ -90,53 +89,55 @@ defmodule HillClimbingAlgorithmTest do
     assert map.start == {0, 20}
     assert map.target == {138, 20}
     path = find_shortest_path(map)
-    assert Enum.count(path) - 1 == 31
+    assert Enum.count(path) - 1 == 440
   end
 
   def find_shortest_path(map),
     do:
       find_shortest_path(
-        PriorityQueue.new() |> PriorityQueue.put(0, [map.start]),
+        [[map.start]],
         map,
         MapSet.new()
       )
 
-  def find_shortest_path(pq, map, seen) do
-    if PriorityQueue.empty?(pq) do
-      raise "FOUND NOTHING!"
-    else
-      do_find_shortest_path(pq, map, seen)
+  def find_shortest_path([], _, _), do: raise("FOUND NOTHING!")
+
+  def find_shortest_path([current_path | other_paths], map, seen) do
+    [last | _] = current_path
+
+    cond do
+      last == map.target ->
+        current_path |> Enum.reverse()
+
+      MapSet.member?(seen, last) ->
+        find_shortest_path(other_paths, map, seen)
+
+      true ->
+        find_shortest_path(
+          other_paths ++ paths_to_visit(map, current_path),
+          map,
+          MapSet.put(seen, last)
+        )
     end
   end
 
-  def do_find_shortest_path(pq, map, seen) do
-    {{current_length, current_path}, pq} = PriorityQueue.pop(pq)
-    [last | remaining] = current_path
-
-    if value_at(map, last) == "E" do
-      current_path |> Enum.reverse()
-    else
-      seen = MapSet.put(seen, last)
+  def paths_to_visit(map, [last | _] = current_path),
+    do:
       candidate_neighbours_positions(map, last)
       |> Enum.filter(fn candidate ->
         last_value = value_at(map, last)
         candidate_value = value_at(map, candidate)
-        valid_step?(last_value, candidate_value) and not MapSet.member?(seen, candidate)
+        valid_step?(last_value, candidate_value)
       end)
-      |> Enum.reduce(pq, fn neighbour, pq ->
-        PriorityQueue.put(pq, {current_length + 1, [neighbour | current_path]})
+      |> Enum.reduce([], fn neighbour, paths_to_visit ->
+        [[neighbour | current_path] | paths_to_visit]
       end)
-      |> find_shortest_path(map, seen)
-    end
-  end
 
-  def valid_step?("S", "a"), do: true
-  def valid_step?("S", _), do: false
-  def valid_step?("z", "E"), do: true
-  def valid_step?(_, "E"), do: false
+  def valid_step?("S", next), do: valid_step?("a", next)
+  def valid_step?(current, "E"), do: valid_step?(current, "z")
 
   def valid_step?(current, next),
-    do: next <= current or :binary.first(current) == :binary.first(next) - 1
+    do: :binary.first(next) - :binary.first(current) <= 1
 
   def value_at(%Map{rows: rows}, {x, y}), do: Enum.at(rows, y) |> Enum.at(x)
 
